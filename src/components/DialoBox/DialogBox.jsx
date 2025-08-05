ba9971dba26c77b87d8a3a85e05e1a4b43bfc7d1
do4best657 <panic_4_what@hotmail.com>
2025-08-05 19:22:21 +0500

    Dialog Button Project is nearly completed and idea is gotten

--- src/components/DialoBox/DialogBox.jsx
+++ src/components/DialoBox/DialogBox.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import {BsFillEmojiFrownFill, BsFillEmojiSmileFill} from "react-icons/bs";
 
 function DialogBox({width=100}) {
     const [subscriptions,setSubscriptions] = useState({
@@ -15,6 +16,19 @@ function DialogBox({width=100}) {
             justifyContent:"justify-center",
         })
     }
+    const [icons,setIcons] = useState("")
+    const handelUnSubscription = ()=>{
+        setSubscriptions({
+            title:"Sorry See you later",
+            description:"You successfully unsubscribed",
+            state:"Unsubscribed",
+            justifyContent:"justify-end",
+        })
+    }
+    useEffect(()=>{
+        subscriptions.state === "Success" && setIcons(<BsFillEmojiSmileFill size={30}/>)
+        subscriptions.state === "Unsubscribed" && setIcons(<BsFillEmojiFrownFill size={30}/>)
+    },[subscriptions.state])
     return (
         <>
 
@@ -24,15 +38,15 @@ function DialogBox({width=100}) {
                     <div className="flex flex-col justify-around items-center">
 <div className="flex flex-row justify-around items-start gap-30">
 <h1 className={"text-xl  "}>{subscriptions.title}</h1>
-    <h1 className={"text-xl "}>Icons</h1>
+    <h1 className={"text-xl "}>{icons}</h1>
 </div>
                     <div className="flex flex-row justify-center items-center ">
                     <span className={" m-5"}>{subscriptions.description}</span>
                     </div>
 <div className={`flex flex-row ${subscriptions.justifyContent} items-center gap-4`}>
-    {subscriptions.state === null && (<button className=" btn btn-primary ">Cancel</button>)}
-    {subscriptions.state === "Success"?null:<button className="btn btn-warning   ">Subscribe</button>}
-    {subscriptions.state === "Success"?<button className="btn btn-warning   ">UnSubscribe</button>:null}
+    {subscriptions.state === null && (<button className=" btn btn-primary">Cancel</button>)}
+    {subscriptions.state === "Success"?null:<button className="btn btn-warning " onClick={handelSubscription}>Subscribe</button>}
+    {subscriptions.state === "Success"?<button className="btn btn-warning   " onClick={handelUnSubscription}>UnSubscribe</button>:null}
 </div>
                 </div>
                 </div>
