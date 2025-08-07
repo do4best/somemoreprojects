94f2487411966de3d07cca10e5dfd259d15eaa1a
do4best657 <panic_4_what@hotmail.com>
2025-08-07 13:29:12 +0500

    Main To Do App main page is nearly completed

--- src/components/mainTodoApp/Landing/landing.jsx
+++ src/components/mainTodoApp/Landing/landing.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import NavigationComponent from "../navigationComponent.jsx";
+import {Link} from "react-router-dom";
+import img1 from "./mytodo.jpeg"
 
 function Landing(props) {
     return (
-        <></>
+        <>
+        <div >
+            <NavigationComponent/>
+            <div className=" flex flex-row justify-between items-center flex-nowrap gap-2 mt-50">
+                <div>
+                <h1 className={"text-8xl font-extrabold"}>Schedule your Task with <span className={"text-success"}>Todos</span></h1>
+                <div className={"flex flex-row gap-5 mt-10 justify-center items-center"}>
+                    <Link to={"/register"} className={"btn btn-lg btn-success"}>Register</Link>
+                    <Link to={"/login"} className={"btn btn-lg btn-primary"}>Login</Link>
+                </div>
+            </div>
+            <div className="">
+                <img src={img1} alt="" className={"w-200 h-100 mr-20 rounded-3xl"}/>
+            </div>
+           </div>
+        </div>
+        </>
     );
 }
 
---
+++ src/components/mainTodoApp/navigationComponent.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+function NavigationComponent(props) {
+    return (
+        <>
+
+            <div className="navbar bg-base-100 shadow-sm">
+                <div className="flex-1">
+                    <Link to={"/"} className="btn btn-error text-xl hover:bg-green-600" >To Do's</Link>
+
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+                        <li> <Link to={"/"} className={"active:bg-accent"} >Home</Link></li>
+                        <li>
+                            <Link to={"/login"} >Login</Link>
+                        </li>
+                        <li>
+                            <Link to={"/register"} >Register</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default NavigationComponent;
