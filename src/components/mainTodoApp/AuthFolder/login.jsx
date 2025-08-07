ac3253bd1f2da36a2df0433ae9d6976bb69e46d7
do4best657 <panic_4_what@hotmail.com>
2025-08-07 19:11:50 +0500

    Main To Do App Login Page is developed

--- src/components/mainTodoApp/AuthFolder/login.jsx
+++ src/components/mainTodoApp/AuthFolder/login.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from "react-router-dom";
+import Input from "daisyui/components/input/index.js";
 
 function Login(props) {
+    const [userNames,setUserNames] = useState("")
+    const [password,setPassword] = useState("")
+    const handelSubmit = ()=>{
+        console.log("submit")
+    }
     return (
-        <></>
+        <>
+            <div className="flex flex-col justify-center items-center">
+            <div className="card w-96 bg-base-100 card-lg shadow-2xl shadow-accent mt-50">
+                <div className="card-body flex flex-col justify-center items-center">
+                    <h2 className="card-title text-center border-2 rounded-full p-5">Image</h2>
+                    <h1>Login</h1>
+
+                    <input type="text" placeholder="user name" value={userNames} className="input" onChange={(e)=>setUserNames(e.target.value)}/>
+
+                    <input type={"password"}  placeholder="password" value={password} className="input mt-5 password" onChange={(e)=>setPassword(e.target.value)} />
+
+                        <div className="  flex flex-row justify-around items-center   ">
+                        <div className="">New User?</div>
+                        <div className="p-3 pr-8"><Link to={"/register"}>Register</Link></div>
+                    </div>
+                    <div className="justify-end card-actions">
+                        <button className="btn btn-primary" disabled={!userNames||!password} onClick={handelSubmit}>Login</button>
+                        <button className="btn btn-success ">Register</button>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </>
     );
 }
 
